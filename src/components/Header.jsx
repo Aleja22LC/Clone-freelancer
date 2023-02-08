e2077f021cc892f4b87483ffd889d8af5011b9df
Aleja22LC <alejaleona@hotmail.com>
2023-02-08 10:23:36 -0500

    se presenta problemas con el bootstrap, parece que no encuentra algunos componentes y no lo aplica por lo cual genera error

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import 'bootstrap';
+import '../styles/Header.css';
+import 'bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <div className="container-fluid nav-container">
+      <div className="row">
+        <div className="col-6">
+          <h1>START BOOTSTRAP</h1>
+        </div>
+        <div className="col-6 navbar">
+          <div className="row">
+            <div className="col"><a href="#portfolio">PORTFOLIO</a></div>
+            <div className="col"><a href="#about">ABOUT</a></div>
+            <div className="col"><a href="#contact">CONTACT</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
